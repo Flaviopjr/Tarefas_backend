445179e2dba98c5f852a59dd0ebce46b86744fa3
flavio <flaviojpjr10@gmail.com>
2024-03-21 01:10:37 -0300

    Initial commit

---
+++ src/Tarefas.java
@@ -0,0 +1,15 @@
+
+public class Tarefas {
+    
+    public static void main(String[] args) {
+        int nota1 = 7;
+        int nota2 = 6;
+        int nota3 = 8;
+        int nota4 = 7;
+        int result = (nota2 + nota1 + nota3 + nota4) / 4;
+
+        System.out.print("A média é: ");
+        System.out.print(result);
+
+    }
+}

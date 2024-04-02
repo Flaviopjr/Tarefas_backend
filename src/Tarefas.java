1cb94a8155b46ea80a4e5fdd9792eaf508802710
flavio <flaviojpjr10@gmail.com>
2024-04-02 15:23:31 -0300

    Utilizando Wrappers

--- src/Tarefas.java
+++ src/Tarefas.java
@@ -1,18 +1,18 @@
 /**
  * @author flaviopjr
  */
-
+import java.util.Scanner;
 public class Tarefas {
     
     public static void main(String[] args) {
-        int nota1 = 7;
-        int nota2 = 6;
-        int nota3 = 8;
-        int nota4 = 7;
-        int result = (nota2 + nota1 + nota3 + nota4) / 4;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.println("A sua idade é: " + idade);
 
-        System.out.print("A média é: ");
-        System.out.print(result);
+        System.out.print("Digite a sua idade: ");
+        Integer idade1 = scanner.nextInt();
+        System.out.println("A sua idade é: " + idade1);
 
     }
 }

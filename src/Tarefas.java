c455911ffdc407433d7a5cc8177f6959b31b2f15
flavio <flaviojpjr10@gmail.com>
2024-04-18 01:29:24 -0300

    Utilizando controles de fluxo

--- src/Tarefas.java
+++ src/Tarefas.java
@@ -6,13 +6,29 @@ public class Tarefas {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a sua idade: ");
-        int idade = scanner.nextInt();
-        System.out.println("A sua idade é: " + idade);
 
-        System.out.print("Digite a sua idade: ");
-        Integer idade1 = scanner.nextInt();
-        System.out.println("A sua idade é: " + idade1);
+        System.out.println("Vamos calcular suas notas.");
+        System.out.println("Digite sua primeira nota: ");
+        Integer nota1 = scanner.nextInt();
+        System.out.println("A sua primeira nota é: " + nota1);
+        System.out.println("Digite sua segunda nota: ");
+        Integer nota2 = scanner.nextInt();
+        System.out.println("Sua segunda nota é: " + nota2);
+        System.out.println("Digite a terceira nota: ");
+        Integer nota3 = scanner.nextInt();
+        System.out.println("Sua terceira nota é: " + nota3);
+        System.out.println("Digite sua última nota: ");
+        Integer nota4 = scanner.nextInt();
+        Integer result = nota1 + nota2 + nota3 + nota4 / 4;
 
+    public static String getResult (Integer result) {
+            if (result >= 7) {
+                System.out.println("Você está aprovado.");
+            } else if (result >= 5 && result < 7) {
+                System.out.println("Você está de recuperação.");
+            } else if (result < 5) {
+                System.out.println("Você está reprovado.");
+            }
+        }
     }
 }
